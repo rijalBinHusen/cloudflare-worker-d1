5c697a464c53327a2467118a57320db10bc750ab
rijalbinhusen <jeryzal333@gmail.com>
2025-03-17 15:52:48 +0700

    everything okay, and deployed to cloudflare

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,26 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+// export default {
+// 	async fetch(request, env, ctx): Promise<Response> {
+// 		return new Response('Hello World!');
+// 	},
+// } satisfies ExportedHandler<Env>;
+
+import { PrismaClient } from "@prisma/client";
+import { PrismaD1 } from "@prisma/adapter-d1";
+
+export interface Env {
+	DB: D1Database;
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const adapter = new PrismaD1(env.DB);
+		const prisma = new PrismaClient({ adapter });
+
+		const users = await prisma.user.findMany();
+		const result = JSON.stringify(users);
+		return new Response(result);
 	},
 } satisfies ExportedHandler<Env>;
